992f900e8292c71c780ed2a6b9d1effe9baf7f9a
Darryl <darryltanggy@gmail.com>
2018-04-25 15:41:28 +0800

    countdown function

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -5,13 +5,39 @@ var CountdownForm = require('CountdownForm');
 var Countdown = React.createClass({
     // create an initial state
     getInitialState: function () {
-        return {count: 0};
+        return {
+            count: 0,
+            countdownStatus: 'stopped'
+        };
+    },
+    // when a change in state or props  is detected, this method gets called automatically
+    componentDidUpdate: function (prevProps, prevState) {
+        // if current state is not equal to prev state ('stopped')
+        if (this.state.countdownStatus !== prevState.countdownStatus) {
+            switch (this.state.countdownStatus) {
+                case 'started':
+                    this.startTimer();
+                break;
+            }
+        }
+
+    },
+    startTimer: function() {
+        // every 1 sec, - 1sec from the current count until it reaches 0
+        this.timer = setInterval(() => {
+            var newCount = this.state.count - 1;
+            this.setState({
+                // makes sure that count doesnt go below 0
+                count: newCount >= 0 ? newCount : 0
+            });
+        }, 1000);
     },
     /* when user submits the form, it will change the 'count' state to 
         whatever the user inputted (defined as seconds) */
     handleSetCountdown: function (seconds) {
         this.setState({
-            count: seconds
+            count: seconds,
+            countdownStatus: 'started'
         });
     },
     render: function () {
